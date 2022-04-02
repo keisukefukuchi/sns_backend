6539ea2ea4ceeffc37b20ee7ec78490a61d78114
keisukefukuchi <kekke53530708@gmail.com>
2022-04-02 16:21:36 +0900

    Update: database

--- database/migrations/2022_03_08_234430_create_posts_table.php
+++ database/migrations/2022_03_08_234430_create_posts_table.php
@@ -15,6 +15,10 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('title')->nullable();
+            $table->string('message');
             $table->timestamps();
         });
     }
